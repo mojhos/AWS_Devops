59b1be03f41a10dd0978fe798f804d5f18223466
Mojtaba Hosseini <53704767+mojhos@users.noreply.github.com>
2025-10-16 17:25:39 -0400

    Add AWS ECR login command to buildImage function

--- java-webapp/script.groovy
+++ java-webapp/script.groovy
@@ -5,6 +5,7 @@ def buildWar() {
 
 def buildImage() {
     echo "building the docker image and push it on public ECR repository..."
+    sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f0y2n1c4'
     sh 'docker build -t public.ecr.aws/f0y2n1c4/aws-devops:latest .'
     sh 'docker push public.ecr.aws/f0y2n1c4/aws-devops:latest'
     
