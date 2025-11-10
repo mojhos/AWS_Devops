9fe2488e393fb9af8bc66a2f941d745d71350bab
Arman Dabiri <arman_dabiri@hotmail.com>
2025-11-09 22:23:08 -0800

    fix: networking issue and session manager issue resolved

--- aws-infra/backend.tf
+++ aws-infra/backend.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
-  backend "s3" {
-    bucket         = "terraform-backend-aws-devops"
-    key            = "envs/dev/infra.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "terraform-backend-aws-devops"
+  #   key            = "envs/dev/infra.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "terraform-lock-table"
+  #   encrypt        = true
+  # }
 
   required_providers {
     aws = {
